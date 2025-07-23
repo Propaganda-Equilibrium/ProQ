d988ea844a14dfc809bd304728b8e2e7a5efa11c
Propaganda Equilibrium <maya.urushadze@gmail.com>
2025-07-24 00:30:47 +0400

    Create main.jsx

---
+++ frontend/src/main.jsx
@@ -0,0 +1,9 @@
+/* src/main.jsx */
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import NarrativeScanner from './NarrativeScanner';
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <NarrativeScanner />
+  </React.StrictMode>
